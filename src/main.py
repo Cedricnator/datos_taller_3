67911d970dcc78d8c936fdd444503411564d1ae4
admin-xpertia <info.xpertia@xpertia.cl>
2025-04-30 10:28:58 -0400

    feat[back]: add initial dataset

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
+import pandas as pd
+import csv
 
 def main():
-   print("Hello, World!")
-   
+   with open('./data/dataset/train.csv', 'r') as file:
+      # creating a csv reader object
+      csv_reader = csv.reader(file)
+
+      dataset = pd.DataFrame(csv_reader)
+      
+      entry_data = dataset.head(120)
+      
+      entry_data.to_csv('./data/dataset/entry_data.csv', index=False)
+      
 if __name__ == "__main__":
    main()
